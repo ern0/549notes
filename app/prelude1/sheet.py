8cf1c3bd838eea0b8b599142d2f25ecc3a2bea97
ern0 <ern0@linkbroker.hu>
2018-08-20 22:39:03 +0200

    value calc

--- app/prelude1/sheet.py
+++ app/prelude1/sheet.py
@@ -11,21 +11,52 @@ class Prelude1:
 
 
 		self.fillTextData()
-		lineCount = len(self.textData) / 55
-		print(lineCount)
 
-		self.convertTextToRaw()
+		self.baseRaw = self.convertTextToRaw(self.baseText)
+		self.endRaw = self.convertTextToRaw(self.endText)
+		self.codaRaw = self.convertTextToRaw(self.codaText)
+
+		print(self.baseRaw)		
 
 
+	def convertTextToRaw(self,textArray):
 
-	def convertTextToRaw(self):
+		result = []
+		for text in textArray:
 
-		pass
+			note = text[:-1]
+			octave = text[-1:]
+
+			value = (int(octave) - 1) * 12
+			value += self.note2value(note)
+
+			result.append(value)
+		
+		return result		
+
+
+	def note2value(self,note):
+
+		if note == "c": return 0
+		elif note == "cis": return 1
+		elif note == "d": return 2
+		elif note == "dis": return 3
+		elif note == "e": return 4
+		elif note == "f": return 5
+		elif note == "fis": return 6
+		elif note == "g": return 7
+		elif note == "gis": return 8
+		elif note == "a": return 9
+		elif note == "ais": return 10
+		elif note == "h": return 11
+
+		print("bad note: " + note)
+		quit()
 
 
 	def fillTextData(self):
 
-		self.textData = [
+		self.baseText = [
 
 			"c3","e3","g3","c4","e4",
 			"c3","d3","a3","d4","f4",
@@ -70,15 +101,19 @@ class Prelude1:
 			"c1","c2","g2","ais2","e3",
 		]
 
-		self.textFinish = [
+		self.endText = [
 
 			"c1","c2","f2","a2","c3","f3","c3","a2",
 			"c3","a2","f2","a2","f2","d2","f2","d2",
 			"c1","h1","g3","h3","d4","f4","d4","h3",
 			"d4","h3","g3","h3","d3","f3","e3","d3",
 
-			"c1","c2","e3","g3","c4"
+			"c1"
 
+		]
+
+		self.codaText = [
+			"c1","c2","e3","g3","c4"
 		]
 
 
