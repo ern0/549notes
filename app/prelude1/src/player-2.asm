0df23147ec472c20ea48c486c87778f142b0713d
ern0 <ern0@linkbroker.hu>
2018-10-03 13:47:26 +0200

    delay tail call

--- app/prelude1/src/player-2.asm
+++ app/prelude1/src/player-2.asm
@@ -10,7 +10,7 @@
 ;       Common:
 ;	  AL - param, result: loaded word on LSBs
 ; 	  AH - param, word length loop counter (SHL until zero)
-;         BL - (free)
+;         BL - local, play_note
 ;         BH - local, data sub correction
 ;	  CL - global, latch counter (SHL until zero)
 ;   	  CH - global, latch value
@@ -37,23 +37,21 @@
 @next_note:
 
 	call	load_note
-
 	call	print_note
-	jmp	@nplay
-
+ ;jmp @nplay
 	call	play_note
-	mov	al,6
-	call	delay
 
 @nplay:
 	inc	byte [counter]
-	cmp	byte [counter],20
+	cmp	byte [counter],10
 	jne	@next_note
 
 	int	20H
 
 counter:
 	db 0
+delay:
+	dw 5
 ;#######################################################################
 load_note:
 	mov	bl,DATA_CSUB
@@ -70,7 +68,7 @@ load_note:
 ;adjust_word:
 	sub	al,bl
 
-;rotate:
+;rotate_notes:
 	lea	di,[data_start]
 	add	al,[di]
 	lea	si,[di + 1]
@@ -103,52 +101,49 @@ load_note:
 	jmp	@next_bit
 
 ;-----------------------------------------------------------------------
-play_note: ; parm: AH, local: AL, BH, DX
-
-	mov	bh,90H		; Note On, Channel 1
-	call	play_byte
-
-	mov	bh,ah		; Pitch
-	call	play_byte
-
-	mov	bh,7fH		; Velocity
-	call	play_byte
-
-	ret
-;-----------------------------------------------------------------------
 play_byte:
-
 	mov	dx,331H
 	in	al,dx
 	test	al,40H
 	jnz	play_byte
 
 	dec	dx
-	mov	al,bh
+	mov	al,bl
 	out	dx,al
 
 	ret
 ;-----------------------------------------------------------------------
-delay:
+play_note: ; parm: AL, local: AL, BL, DX
+
 	pusha
 
-	xor	ch,ch
-	mov	cl,al
+	push	ax
+	mov	bl,90H		; Note On, Channel 1
+	call	play_byte
 
-wait_some:
-	push	cx
+	pop	bx		; Pitch
+	call	play_byte
 
+	mov	bl,7fH		; Velocity
+	call	play_byte
+
+	; fall delay
+
+;-----------------------------------------------------------------------
+;delay:
+	mov	si,[delay]
+
+@wait_some:
 	mov	ah,2cH
 	int	21H
 	mov	bl,dl
 
-wait_tick:
-	mov	ah,2cH
+@wait_tick:
 	int	21H
 	cmp	bl,dl
-	je	wait_tick
-	pop	cx
-	loop	wait_some
+	je	@wait_tick
+	dec	si
+	jne	@wait_some
 
 	popa
 	ret
